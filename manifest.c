404a7a96ac9a862f3023de603467c4e9bbc5afee
Nick Bray <ncbray@google.com>
2018-05-31 15:29:22 -0700

    Remove dead include.
    Change-Id: I0fcca5cbcd8da3e0752365e72dc9c39fb96683a0

--- manifest.c
+++ manifest.c
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include <stddef.h>
 #include <trusty_app_manifest.h>
 
 trusty_app_manifest_t TRUSTY_APP_MANIFEST_ATTRS trusty_app_manifest =
