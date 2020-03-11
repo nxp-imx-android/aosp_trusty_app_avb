04886992101077cd4c76acc72f9f1775c6b93d40
Rajesh Nyamagoud <nyamagoud@google.com>
2020-03-10 17:07:48 -0700

    Switch from manifest.c to manifest.json config
    Bug: 130560272
    Change-Id: Ic89f041ad4d2afb5eedc1bbc81f73c0b36e492e3

--- rules.mk
+++ rules.mk
@@ -17,8 +17,9 @@ LOCAL_DIR := $(GET_LOCAL_DIR)
 
 MODULE := $(LOCAL_DIR)
 
+MANIFEST := $(LOCAL_DIR)/manifest.json
+
 MODULE_SRCS += \
-	$(LOCAL_DIR)/manifest.c \
 	$(LOCAL_DIR)/avb_messages.cpp \
 	$(LOCAL_DIR)/avb_manager.cpp \
 	$(LOCAL_DIR)/secure_storage.cpp \
